4357adc53e8a41d68289a6e828cfeebf64509654
GollllDong <wjdqjatnwkd@naver.com>
2024-01-06 16:51:04 +0900

    2st commit

--- Git.java
+++ Git.java
@@ -5,3 +5,10 @@
 // git branch -M main
 // git remote add origin https://github.com/GollllDong/Study.git
 // git push -u origin main
+
+// 수정된 코드 올릴때
+// git add .    새롭게 추가하거나 변경한 코드 전부 올린다.
+// git status  올릴 코드들 뭐있는지 확인
+// git commit -m "커밋할 메시지"  수정하면서 올릴 내용
+// git remote set-url origin https://github.com/GollllDong/Study.git 이미 설정되어있다면 생략 가능
+// git push origin main 내 깃헙으로 전송
