9a4d2eadff08aa12a8f92c2bdd595af8cd193241
GollllDong <wjdqjatnwkd@naver.com>
2024-01-08 21:16:38 +0900

    2st commite

--- Ch3/OperatorEx15.java
+++ Ch3/OperatorEx15.java
@@ -3,7 +3,6 @@ class OperatorEx15 {
       public static void main(String[] args) { 
             char lowerCase = 'a'; 
             char upperCase = (char)(lowerCase - 32); 
-
-            System.out.println(upperCase); 
+            System.out.println("a : "+ upperCase); 
       } 
 } 
--- asdasd.java
+++
@@ -1,5 +0,0 @@
-class Asdasd {
-    public static void main(String[] args) {
-        System.out.println("Hi");
-    }
-}
