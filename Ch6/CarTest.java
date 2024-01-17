d65f0c838e72e72fa9a8d7b9d9e69b4a08c36c6e
GollllDong <wjdqjatnwkd@naver.com>
2024-01-17 23:58:35 +0900

    4st commit

--- Ch6/CarTest.java
+++ Ch6/CarTest.java
@@ -1,30 +1,30 @@
-package Ch6;
+// package Ch6;
 
-class Car {
-    String color;
-    String brand;
-    int pay;
+// class Car {
+//     String color;
+//     String brand;
+//     int pay;
 
-    Car () {} // C1 
+//     Car () {} // C1 
 
-    Car(String c, String b, int p) { // C2 >> IV, LV 유의
-        color = c;
-        brand = b;
-        pay = p;
-    }
-}
+//     Car(String c, String b, int p) { // C2 >> IV, LV 유의
+//         color = c;
+//         brand = b;
+//         pay = p;
+//     }
+// }
 
-class CarTest {
-    public static void main(String[] args) {
+// class CarTest {
+//     public static void main(String[] args) {
         
-        Car c1 = new Car();
-        c1.color = "white";
-        c1.brand = "KIA";
-        c1.pay = 4200;
-        System.out.printf("Color : "+c1.color+"%nBrand : "+c1.brand+"%nPay : "+ c1.pay+"%n");
-        System.out.println("--------------------------");
-        Car c2 = new Car("Black", "Hyundai", 5000);
-        System.out.printf("Color : "+c2.color+"%nBrand : "+c2.brand+"%nPay : "+ c2.pay);
-    }
+//         Car c1 = new Car();
+//         c1.color = "white";
+//         c1.brand = "KIA";
+//         c1.pay = 4200;
+//         System.out.printf("Color : "+c1.color+"%nBrand : "+c1.brand+"%nPay : "+ c1.pay+"%n");
+//         System.out.println("--------------------------");
+//         Car c2 = new Car("Black", "Hyundai", 5000);
+//         System.out.printf("Color : "+c2.color+"%nBrand : "+c2.brand+"%nPay : "+ c2.pay);
+//     }
     
-}
+// }
