fd33bf314be7c7d29553be047266b60f11629c68
BS Jeong <wjdqjatnwkd@naver.com>
2024-01-17 21:06:01 +0900

    2st commit(vsCode X, EClipse O)

---
+++ Ch6/CarTest.java
@@ -0,0 +1,30 @@
+package Ch6;
+
+class Car {
+    String color;
+    String brand;
+    int pay;
+
+    Car () {} // C1 
+
+    Car(String c, String b, int p) { // C2
+        color = c;
+        brand = b;
+        pay = p;
+    }
+}
+
+class CarTest {
+    public static void main(String[] args) {
+        
+        Car c1 = new Car();
+        c1.color = "white";
+        c1.brand = "KIA";
+        c1.pay = 4200;
+        System.out.printf("Color : "+c1.color+"%nBrand : "+c1.brand+"%nPay : "+ c1.pay+"%n");
+        System.out.println("--------------------------");
+        Car c2 = new Car("Black", "Hyundai", 5000);
+        System.out.printf("Color : "+c2.color+"%nBrand : "+c2.brand+"%nPay : "+ c2.pay);
+    }
+    
+}
