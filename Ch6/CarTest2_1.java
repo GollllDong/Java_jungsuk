f3ed4299634fbfa3344c02b4ccf6120f84f4f545
BS Jeong <wjdqjatnwkd@naver.com>
2024-01-17 22:13:48 +0900

    2st commit(vsCode X, EClipse O)

--- Ch6/CarTest2_1.java
+++ Ch6/CarTest2_1.java
@@ -3,7 +3,7 @@ package Ch6;
 class Car {
 	String color;		// 색상
 	String gearType;	// 변속기 종류 - auto(자동), manual(수동)
-	int door;			// 문의 개수
+	int door;			// 차 문 개수
 
 	Car() {
 		this("white", "auto", 4);	
