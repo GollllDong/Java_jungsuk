6584b43bed56874db835f19dcdbb529cda5bc351
BS Jeong <wjdqjatnwkd@naver.com>
2024-01-19 10:25:21 +0900

    3st commit

--- Ch6/ReferenceReturnEx.java
+++ Ch6/ReferenceReturnEx.java
@@ -1,21 +1,21 @@
 package Ch6;
 
-class Data { int x; }
+class Data_re { int x; }
 
 class ReferenceReturnEx {
 	public static void main(String[] args) 
 	{
-		Data d = new Data();    // d라는 참조변수에 0x100번지에 x라는 값을 가진 객체 생성
+		Data_re d = new Data_re();    // d라는 참조변수에 0x100번지에 x라는 값을 가진 객체 생성
 		d.x = 10;               // 0x100번지에 x값 10 할당
 
-		Data d2 = copy(d);      // 참조변수 d의 주소번지를 매개변수로 할당시켜줌 
+		Data_re d2 = copy(d);      // 참조변수 d의 주소번지를 매개변수로 할당시켜줌 
         // >> 반환된 10의 값이 tmp의 주소번지까지 따라가게 되면서 d2는 0x200번지를 가짐. 
 		System.out.println("d.x ="+d.x);
 		System.out.println("d2.x="+d2.x);
 	}
 
-	static Data copy(Data d) { // 참조형 변수이므로 d의 주소번지 0x100번지가 할당된다.
-		Data tmp = new Data(); // tmp라는 참조변수에 0x200번지의 x라는 값을 가진 객체 생성
+	static Data_re copy(Data_re d) { // 참조형 변수이므로 d의 주소번지 0x100번지가 할당된다.
+		Data_re tmp = new Data_re(); // tmp라는 참조변수에 0x200번지의 x라는 값을 가진 객체 생성
 		tmp.x = d.x;           // 0x100번지의 참조변수 d의 x의값인 10을 0x200번지에 복사
 		return tmp;            // tmp가 가진 x의 값 10을 copy(d)의 값으로 반환
 	}
