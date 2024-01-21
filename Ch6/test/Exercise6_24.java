874a4a23efa9977ebfa9c5386529d14573c463a7
BS Jeong <wjdqjatnwkd@naver.com>
2024-01-21 16:43:28 +0900

    2st commit(fix absolute value)

--- Ch6/test/Exercise6_24.java
+++ Ch6/test/Exercise6_24.java
@@ -1,8 +1,8 @@
 package Ch6.test;
 
 class Exercise6_24 {
-    static int abs(int value) {
-        return value;
+    public static int abs(int value) {
+        return value < 0 ? -value : value;
     }
     public static void main(String[] args) {
         int value = 5;
