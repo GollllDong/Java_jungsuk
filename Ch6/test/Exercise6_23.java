2a419531aeb099cec2438eb40a442aeb0b901a44
BS Jeong <wjdqjatnwkd@naver.com>
2024-01-21 16:37:33 +0900

    2st commit(Space fit)

--- Ch6/test/Exercise6_23.java
+++ Ch6/test/Exercise6_23.java
@@ -5,19 +5,20 @@ public class Exercise6_23 {
         
         int[] data = {3,2,9,4,7};
         System.out.println(java.util.Arrays.toString(data));
-        System.out.println("최대값 :"+max(data)); 
-        System.out.println("최대값 :"+max(null)); 
-        System.out.println("최대값 :"+max(new int[]{})); // 0 최대값 크기가 인 배열
+        System.out.println("최대값 : "+max(data)); 
+        System.out.println("최대값 : "+max(null)); 
+        System.out.println("최대값 : "+max(new int[]{})); // 0 최대값 크기가 인 배열
     }
-    static int max (int[] arr) {
-        if(arr == null || arr.length == 0) {
+    static int max(int[] arr) {
+        if (arr == null || arr.length < 1) {
             return -999999;
         }
         int tmp = arr[0];
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if(arr[i] > tmp) {
                 tmp = arr[i];
             }
-        }return tmp;
+        }
+        return tmp;
     }
 }
