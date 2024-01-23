91c9a6e2cb9b977d145e4d668ccc6e723bc549ec
BS Jeong <wjdqjatnwkd@naver.com>
2024-01-23 21:26:05 +0900

    2st commit(encoding)

--- Ch5/ArrayEx3.java
+++ Ch5/ArrayEx3.java
@@ -8,7 +8,7 @@ public class ArrayEx3 {
             for(int i=0; i < arr.length;i++) 
                 arr[i] = i + 1;
     
-            System.out.println("������ - arr.length:"+arr.length);	
+            System.out.println("변경전- arr.length:"+arr.length);	
             for(int i=0; i < arr.length;i++) 
                 System.out.println("arr["+i+"]:"+arr[i]);	
     
@@ -20,7 +20,7 @@ public class ArrayEx3 {
     
             arr = tmp;  // tmp에 저장된 값을 arr에 저장한다.
     
-            System.out.println("������ - arr.length:"+arr.length);	
+            System.out.println("변경후- arr.length:"+arr.length);	
             for(int i=0; i < arr.length;i++) 
                 System.out.println("arr["+i+"]:"+arr[i]);	
         }
